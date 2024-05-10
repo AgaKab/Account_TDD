e06f1f15557a74bc0cdd7d21e25488ead5f5b64a
AgaKab <agata,kabot@vp.pl>
2024-05-10 10:54:50 +0200

    ValueError tests added

--- account_test.py
+++ account_test.py
@@ -12,10 +12,17 @@ class TestAccount(unittest.TestCase):
         self.account.deposit(50)
         self.assertEqual(100, self.account.get_balance())
         
+    def test_negative_deposit(self):
+        with self.assertRaises(ValueError):
+            self.account.deposit(-50)
+        
     def test_withdraw(self):
         self.account.withdraw(20)
         self.assertEqual(30, self.account.get_balance())
-        
+    
+    def test_withdraw_isufficient_funds(self):
+        with self.assertRaises(ValueError):
+            self.account.withdraw(100)        
 
 if __name__=="__main__":
     unittest.main()
